0cc2dcdcfe0f762f0f776260457b0bcfc85dc539
somil <somil.shah@quantiphi.com>
2021-12-21 02:49:26 +0530

    returning error

--- services/firebaseServices.js
+++ services/firebaseServices.js
@@ -21,7 +21,7 @@ export default {
         const collectionLocation = collection(db, collectionID)
         try {
             return await addDoc(collectionLocation, data) //(parameter) response: DocumentReference<any>
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async addDocumentManualID(collectionID, custom_documentID, data) { // adds a document with ID in the collection
@@ -29,14 +29,14 @@ export default {
         try {
             await setDoc(documentLocation, data)
             return "Document Added Successfully With Manual ID"
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async getSingleDocumentByID(collectionID, documentID) { //get any single document from the collection
         const documentLocation = doc(db, collectionID, documentID);
         try {
             return await getDoc(documentLocation) //(parameter) response: DocumentSnapshot<DocumentData>
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async updateSingleDocument(collectionID, documentID, newdata) { //update any document in a collection
@@ -44,7 +44,7 @@ export default {
         try {
             await updateDoc(documentLocation, newdata)  //(parameter) response: void
             return "Document Updated Successfully"
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async addArrayElement(collectionID, documentID, key, value) {
@@ -52,7 +52,7 @@ export default {
         try {
             await updateDoc(documentLocation, { [key]: arrayUnion(value) }) //(parameter) response: void
             return "Item Added Successfully"
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async removeArrayElement(collectionID, documentID, key, value) {
@@ -60,7 +60,7 @@ export default {
         try {
             await updateDoc(documentLocation, { [key]: arrayRemove(value) }) //(parameter) response: void
             return "Item Removed Successfully"
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async deleteSingleDocument(collectionID, documentID) { //deletes a specific document from the collection
@@ -68,27 +68,27 @@ export default {
         try {
             await deleteDoc(documentLocation) //(parameter) response: void
             return "Document Deleted Successfully"
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async getAllDocuments(collectionID) { //reads all the documents in a collection
         const collectionLocation = collection(db, collectionID)
         try {
             return await getDocs(collectionLocation)//(parameter) response: DocumentReference<any>
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async getSingleMedia(media_path) { // get url for a single media file
         const downloadLocation = ref(storage, media_path);
         try {
             return await getDownloadURL(downloadLocation) //(parameter) url: string
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async setSingleMedia(media_path, file) { // add single media to storage
         const uploadLocation = ref(storage, media_path);
         try {
             return await uploadBytes(uploadLocation, file) //(parameter) snapshot: UploadResult
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     }
 }
--- services/postPropertyServices.js
+++ services/postPropertyServices.js
@@ -4,7 +4,7 @@ export default {
     async postConstructionDetails(collectionID, data) {
         try {
             return await firebaseServices.addDocumentAutoID(collectionID, data).catch((err) => { console.error(err); })
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postLocationDetails(collectionID, documentID, newdata) {
@@ -15,25 +15,25 @@ export default {
                 await firebaseServices.addArrayElement('localities', newdata['location_details']['locality'], 'properties', documentID), //update localities array
                 await firebaseServices.addArrayElement('sub_localities', newdata['location_details']['sub_locality'], 'properties', documentID) //update sub_localities array
             ]
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postPropertyDetails(collectionID, documentID, newdata) {
         try {
             return await firebaseServices.updateSingleDocument(collectionID, documentID, newdata) //update property
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postPricingDetails(collectionID, documentID, newdata) {
         try {
             return await firebaseServices.updateSingleDocument(collectionID, documentID, newdata) //update property
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postAmenitiesDetails(collectionID, documentID, newdata) {
         try {
             return await firebaseServices.updateSingleDocument(collectionID, documentID, newdata) //update property
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postImage(collectionID, documentID, file, metadata, image_path) {
@@ -42,19 +42,19 @@ export default {
                 await firebaseServices.setSingleMedia(image_path, file), //update property
                 await firebaseServices.addArrayElement(collectionID, documentID, 'photos', metadata['photos'][0]), //update photos array
             ]
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postVisitPreferenceDetails(collectionID, documentID, newdata) {
         try {
             return await firebaseServices.updateSingleDocument(collectionID, documentID, newdata) //update property
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
     async postOtherDetails(collectionID, documentID, newdata) {
         try {
             return await firebaseServices.updateSingleDocument(collectionID, documentID, newdata) //update property
-        } catch (error) { console.error(error); }
+        } catch (error) { console.error(error); return error }
     },
 
 }
