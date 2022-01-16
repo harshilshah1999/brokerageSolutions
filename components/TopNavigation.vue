418e192761ab153dabd23e7f94c7bf4a87f92c3c
Harshil Shah <bgharshilshah@gmail.com>
2022-01-16 17:35:16 +0530

    navigation bar and stepper

---
+++ components/TopNavigation.vue
@@ -0,0 +1,138 @@
+<template>
+<div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dark
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Whiteline Realtors</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="ma-1"
+        to="/"
+        plain
+      >
+        
+        <v-icon
+          
+          left
+        >
+          mdi-home
+        </v-icon>
+        Home
+      </v-btn>
+            <v-btn
+        class="ma-1"
+       plain
+       to="/post-property"
+      >
+       
+        <v-icon
+          
+          left
+        >
+          mdi-archive-plus
+        </v-icon>
+         Post Property
+      </v-btn>
+        <v-btn
+        class="ma-1"
+        plain
+      >
+        
+        <v-icon
+          
+          left
+        >
+          mdi-phone
+        </v-icon>
+        Contact Us
+      </v-btn>
+
+       <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+         
+          icon
+          
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-face-man</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+</div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+       items: [
+        { title: 'Favourites' },
+        { title: 'Viewed Properties' },
+        { title: 'Visited Propeties' },
+        { title: 'Enquired Properties' },
+        { title: 'Suggested Propeties' },
+        { title: 'Logout' },
+      ]
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/post-property.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-stepper v-model="e1" alt-labels>
+    <v-stepper-header>
+    <template v-for="n in stepperData">
+      <v-stepper-step
+        :key="`${n.number}-step`"
+        :complete="e1 > n.number"
+        :step="n.number"
+        
+      >
+        {{n.title}}
+      </v-stepper-step>
+
+      <v-divider 
+        v-if="n.number !== 6"
+        :key="n">
+        </v-divider>
+    </template>
+
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card
+          class="mb-12"
+          elevation="0"         
+        >
+           <location-details />
+        </v-card>
+
+        <v-btn
+          color="primary"
+          @click="e1 = 2"
+        >
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
+
+        <v-btn
+          color="primary"
+          @click="e1 = 3"
+        >
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
+
+        <v-btn
+          color="primary"
+          @click="e1 = 1"
+        >
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+</template>
+
+<script>
+import LocationDetails from '../components/stepperForms/LocationDetails.vue';
+
+export default {
+    components:{
+        LocationDetails
+    },
+     data () {
+      return {
+        e1: 1,
+        stepperData: [
+            {
+                title: 'Location Details',
+                number: 1
+            },
+            {
+                title: 'Construction Details',
+                number: 2
+            },
+            {
+                title: 'Propert Details',
+                number: 3
+            },
+            {
+                title: 'Amenities',
+                number: 4
+            },
+            {
+                title: 'Media',
+                number: 5
+            },
+            {
+                title: 'Preferences',
+                number: 6
+            }
+        ]
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
