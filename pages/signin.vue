f8796ebb4e27bde9b18615de52174d714c79bf41
somil <somil.shah@quantiphi.com>
2022-01-31 17:10:31 +0530

    login with phone number completed, yet to be tested for bugs

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,77 +1,39 @@
 <template>
   <div>
     <h1>Login</h1>
-    <br /><br />
-    <form @submit.prevent="submit">
-      <div>
-        <label
-          for="phonenumber"
-          style="font:family:system-ui;font-size: x-large;"
-          >Phone Number</label
-        >
-      </div>
-      <div>
-        <input
-          type="tel"
-          id="phonenumber"
-          maxlength="13"
-          required
-          v-model="phoneNumber"
-          style="
-            text-align: center;
-            maxwidth: 300px;
-            width: 300px;
-            height: 40px;
-          "
-          placeholder="+91 NUMBER"
-          title="Enter Phone number with the code"
-        />
-      </div>
-      <br />
+    <br />
+    <div>
+      <v-text-field
+        v-model="phoneNumber"
+        label="Phone Number"
+        id="phonenumber"
+        maxlength="13"
+        placeholder="+919876543210"
+        required
+        outlined
+      ></v-text-field>
       <div
         id="recaptcha-container"
         style="background-color: #1b1a1a; width: 300px; margin: auto"
       ></div>
-      <br />
-      <div>
-        <button class="btn btn-primary" id="log-in" type="submit">Login</button>
-      </div>
-    </form>
-    <br /><br />
-
-    <br /><br />
+      <v-btn id="log-in" @click="submit">Login</v-btn>
+    </div>
+    <br />
+    <v-divider></v-divider>
+    <br />
     <div>
-      <div>
-        <label style="font:family:system-ui;font-size: x-large;"
-          >Enter Otp</label
-        >
-      </div>
-      <br />
-      <div>
-        <input
-          type="text"
-          id="otpField"
-          style="text-align: center"
-          pattern="[0-9]{6}"
-          maxlength="6"
-          v-model="otpnum"
-          placeholder="Enter OTP"
-          required
-        />
-      </div>
-      <br />
-      <div>
-        <button class="btn btn-primary" @click="verifyCode" id="otp-btn">
-          Confirm
-        </button>
-      </div>
+      <v-otp-input
+        v-model="OTP"
+        required
+        length="6"
+        type="number"
+      ></v-otp-input>
+      <v-btn @click="verifyCode" id="otp-btn"> Confirm OTP </v-btn>
     </div>
   </div>
 </template>
 
 <script>
-// import { getAuth, RecaptchaVerifier } from 'firebase/auth'
-
 import {
   auth,
   signInWithPhoneNumber,
@@ -82,9 +44,11 @@ export default {
   name: 'login',
   data() {
     return {
+      testNumber: '+19999999999',
       phoneNumber: '+19999999999',
       confirmationResult: null,
-      otpnum: '000000',
+      testOTP: '000000',
+      OTP: '000000',
       recaptchaVerifier: null,
       recaptchaWidgetId: null,
       confirmResult: null,
@@ -97,27 +61,23 @@ export default {
       {
         size: 'invisible',
         callback: (response) => {
-          // reCAPTCHA solved, allow signInWithPhoneNumber.
-          onSignInSubmit()
+          alert('reCAPTCHA cleared')
+        },
+        'expired-callback': () => {
+          // Response expired. Ask user to solve reCAPTCHA again.
+          this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+            'recaptcha-container',
+            {},
+            auth
+          )
         },
       },
       auth
     )
-    this.submit()
   },
   methods: {
     submit() {
-      this.recaptchaVerifier = new RecaptchaVerifier(
-        'recaptcha-container',
-        {},
-        auth
-      )
-      this.recaptchaVerifier.render().then((widgetId) => {
-        this.recaptchaWidgetId = widgetId
-      })
-
-      var number = this.phoneNumber
-      signInWithPhoneNumber(auth, number, this.recaptchaVerifier)
+      signInWithPhoneNumber(auth, this.phoneNumber, this.recaptchaVerifier)
         .then((confirmationResult) => {
           this.confirmResult = confirmationResult
           console.log(this.confirmResult)
@@ -130,19 +90,21 @@ export default {
     },
     verifyCode() {
       this.confirmResult
-        .confirm(this.otpnum)
+        .confirm(this.OTP)
         .then((result) => {
           alert('Registeration Successfull!', result)
-          this.gotonext()
+          this.redirectFunction()
           var user = result.user
           console.log(user)
         })
         .catch((error) => {
-          console.log(error)
+          alert('Invalid OTP')
+          console.error(error)
         })
     },
-    gotonext() {
-      this.$router.replace({ name: 'home' })
+    redirectFunction() {
+      alert('You will be redirected')
+      // this.$router.replace({ name: 'home' })
     },
   },
 }
